291e696343648880a91ef71327439bee797d165b
Faqih Arifian <faqiharifianaji@gmail.com>
2016-10-15 06:52:31 +0700

    done

--- app/src/main/java/com/arifian/myappportofolio/MainActivity.java
+++ app/src/main/java/com/arifian/myappportofolio/MainActivity.java
@@ -1,7 +1,9 @@
 package com.arifian.myappportofolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonPressed(View view){
+        String message = "This button will launch my ";
+        switch(view.getId()){
+            case R.id.app1_button:
+                message += getResources().getString(R.string.app_1);
+                break;
+            case R.id.app2_button:
+                message += getResources().getString(R.string.app_2);
+                break;
+            case R.id.app3_button:
+                message += getResources().getString(R.string.app_3);
+                break;
+            case R.id.app4_button:
+                message += getResources().getString(R.string.app_4);
+                break;
+            case R.id.app5_button:
+                message += getResources().getString(R.string.app_5);
+                break;
+            case R.id.app6_button:
+                message += getResources().getString(R.string.app_6);
+                break;
+        }
+        message += " app!";
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
